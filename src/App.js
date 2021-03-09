8470f07a93128d232fd035e1c93c5fef27bf9280
Attunuru <am-samarasimha.reddy@hpe.com>
2021-03-09 17:58:35 +0530

    rountes and navs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom'
-
+import Navs from './components/Navs';
 
 import './App.css';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
-  return( <Switch>
+  return( 
+  <div>
+    <Navs/>
+  <Switch>
     <Route exact path="/">
-         This is home page
+        <Home/>
     </Route>
-    <Route exact path="/contacts">
-      This is contacts page
+    <Route exact path="/starred">
+   <Starred/>
     </Route>
     <Route >
       404 Not found
     </Route>
 
-    </Switch>);
+    </Switch>
+    </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Starred.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Starred() {
+    return (
+        <div>
+            This is starred page
+        </div>
+    )
+};
+
+export default Starred
